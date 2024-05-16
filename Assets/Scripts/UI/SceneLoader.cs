2f130bb2eb36aee10e6838ae2947e216eb6a86b4
None <120342992+Siofr@users.noreply.github.com>
2024-05-16 23:06:15 +0100

    Added Quit Menu
    Title

--- Assets/Scripts/UI/SceneLoader.cs
+++ Assets/Scripts/UI/SceneLoader.cs
@@ -5,20 +5,13 @@ using UnityEngine.SceneManagement;
 
 public class SceneLoader : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
     public void LoadGame()
     {
         SceneManager.LoadScene("Test Map");
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
     }
 }
