0b3106988a20eeced50d35d869458baf6fbf9a06
Funnymike09 <mike.nip@mail.ru>
2024-05-17 03:03:15 +0500

    Ingredient stats Ver1

---
+++ Assets/StatusProgressBar.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.WSA;
+
+public class StatusProgressBar : MonoBehaviour
+{
+    private bool isActive = false;
+    private float IndicatorTimer;
+    private float maxIndicator;
+
+    private Image Bar;
+
+    private void Awake()
+    {
+        Bar = GetComponent<Image>();   
+    }
+
+
+    public void ActivateCD(float CDT)
+    {
+        isActive = true;
+        maxIndicator = CDT;
+        IndicatorTimer = maxIndicator;
+    }
+    public void DeactivateCD()
+    {
+        isActive  = false;
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isActive)
+        {
+            IndicatorTimer -= Time.deltaTime;
+            Bar.fillAmount = (IndicatorTimer / maxIndicator);
+            if (IndicatorTimer <= 0)
+            {
+                DeactivateCD();
+            }
+        }
+    }
+}
